2e07d9f2b63a7c25d9e416dc0db22993f2867865
peterdeme <snypox@gmail.com>
2017-12-06 14:53:18 +0100

    Added Kubernetes

--- main.go
+++ main.go
@@ -5,10 +5,14 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
 	http.HandleFunc("/", helloWorldHandler)
+	http.HandleFunc("/crash", crashAppHandler)
+	go waitForStoppage()
 	fmt.Println("Starting the server.")
 	http.ListenAndServe(":5000", nil)
 }
@@ -16,4 +20,16 @@ func main() {
 func helloWorldHandler(resp http.ResponseWriter, req *http.Request) {
 	hostname, _ := os.Hostname()
 	io.WriteString(resp, fmt.Sprintf("Hello world from %s - app version: %s", hostname, "v1"))
+}
+
+func crashAppHandler(resp http.ResponseWriter, req *http.Request) {
+	fmt.Println("Commiting suicide. Bye-bye")
+	os.Exit(1)
+}
+
+func waitForStoppage() {
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	fmt.Println("Stopping... Got signal:", <-signalChan)
+	os.Exit(0)
 }
